c975f809ea205b791f80832d5d2db5976a5361c5
Sakshi Tripathi <saktripathi@deloitte.com>
2022-11-22 13:20:51 +0530

    cube and cuboid formation

--- index.vr.js
+++ index.vr.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {
   AppRegistry,
   asset,
+  Box,
   Pano,
   Text,
   View,
@@ -10,51 +11,51 @@ import {
 
 export default class WelcomeToVR extends React.Component {
 
-  constructor(props){
-    super(props);
-    this.state=
-    {
-      Txt:"Hello World",
-      imgSrc:"chess-world.jpg"
-    };
-    this.i=0
-  }
-  start(){
-    if(this.i % 2 ==0){
-      this.setState({
-        Txt:"State1",
-        imgSrc:"chess-world.jpg"
-    }) 
-  }
-  else {
-    this.setState({
-      Txt:"state2",
-      imgSrc:"env.jpg"
-    })
-  }
-  this.i++;
-  }
+  // constructor(props){
+  //   super(props);
+  //   this.state=
+  //   {
+  //     Txt:"Hello World",
+  //     imgSrc:"chess-world.jpg"
+  //   };
+  //   this.i=0
+  // }
+  // start(){
+  //   if(this.i % 2 ==0){
+  //     this.setState({
+  //       Txt:"State1",
+  //       imgSrc:"chess-world.jpg"
+  //   }) 
+  // }
+  // else {
+  //   this.setState({
+  //     Txt:"state2",
+  //     imgSrc:"env.jpg"
+  //   })
+  // }
+  // this.i++;
+  // }
 
   render() {
     return (
       <View>
-        <Pano source={asset(this.state.imgSrc)}/>
-        <VrButton onClick={() => this.start()}>
-        <Text
+        <Pano source={asset("env.jpg")}/>
+        {/* <VrButton onClick={() => this.start()}> */}
+        <Box
+        dimWidth={0.35}
+        dimHeight={0.35}
+        dimDepth={0.35}
+        // wireframe={true}
+
+        texture={asset("chess-world.jpg")}
           style={{
-            backgroundColor: '#777879',
-            fontSize: 0.8,
-            fontWeight: '400',
-            layoutOrigin: [0.5, 0.5],
-            paddingLeft: 0.2,
-            paddingRight: 0.2,
-            textAlign: 'center',
-            textAlignVertical: 'center',
-            transform: [{translate: [0, 0, -3]}],
+            color:'white',
+            transform:[{translate:[0,0,-2]},{rotateX:45},{rotateY:50},{scale:[2,0.7,1.5]}]
+
           }}>
-          {this.state.Txt}
-        </Text>
-        </VrButton>
+         
+        </Box>
+        {/* </VrButton> */}
       </View>
     );
   }
