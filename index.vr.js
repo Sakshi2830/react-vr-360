1667f0482ce2ce4403662cbf53008bea87ea7663
Sakshi Tripathi <saktripathi@deloitte.com>
2022-11-23 14:58:32 +0530

    animation timing using spin attribute

--- index.vr.js
+++ index.vr.js
@@ -1,58 +1,76 @@
 import React from 'react';
-import {
-  AppRegistry,
-  asset,
-  Box,
-  Cylinder,
-  Pano,
-  Sphere,
-  Text,
-  View,
-  VrButton,
-} from 'react-vr';
+import {  AppRegistry,  asset,  Pano,  Text,  View,  VrButton,  Box,  Cylinder,  Sphere,  Animated,} from 'react-vr';
+import { Easing } from 'react-native';
 
-export default class WelcomeToVR extends React.Component {
-constructor(){
-  super();
-  this.state={buttonText:"Default Button"};
-}
-  triggerEnter(){
-    this.setState({buttonText:"Mouse Enter"})
-  }
-  triggerExit(){
-    this.setState({buttonText:"Mouse Exit"})
-  }
-  triggerClick(){
-    this.setState({buttonText:"Mouse click"})
-  }
-  triggerLongClick(){
-    this.setState({buttonText:"come on byr byr now"})
+
+export default class animation extends React.Component
+ {
+  constructor()
+   {
+    super();
+    this.state = { spinY: new Animated.Value(0) };
   }
 
+  spinAnimation()
+   {
+    this.state.spinY.setValue(0);
+
+    Animated.timing( this.state.spinY, {toValue: 1,  duration: 3000, easing: Easing.linear} )
+    .start( () => this.spinAnimation() );
+  }
+
+  componentDidMount()
+  {
+    this.spinAnimation();
+  }
+
+  
   render() {
+    const spinY = this.state.spinY.interpolate({inputRange: [0, 1], outputRange: ['0deg', '360deg'] });
+
+    const AnimatedBox = Animated.createAnimatedComponent(Box);
+    const AnimatedSphere = Animated.createAnimatedComponent(Sphere);
+    const AnimatedCylinder = Animated.createAnimatedComponent(Cylinder);
+
     return (
       <View>
-        <Pano source={asset("env.jpg")}/>
-        <VrButton 
-        onEnter={this.triggerEnter.bind(this)}
-        onExit={this.triggerExit.bind(this)}
-        onClick={this.triggerClick.bind(this)}
-        onLongClick={this.triggerLongClick.bind(this)}
-        > 
-      <Text
-      style={{
-        fontSize:0.3,
-        textAlign:"center",
-        backgroundColor:"red",
+      <Pano source={asset("chess-world.jpg")}/>
+      <AnimatedBox
+        dimWidth={0.35}
+        dimDepth={0.35}
+        dimHeight={0.35}
+        texture={asset("chess-world.jpg")}
+        style={{
+          color: '#ffffff',
+          transform: [{translate: [0,0,-2]}, {rotateX: 45}, {rotateY: spinY}, {scale: [1, 1, 1]}],
+        }}
+      />
 
-        transform:[{translate:[-1,0,-3]}]
-      }}>
-        {this.state.buttonText}
-      </Text>
-         </VrButton>
+      <AnimatedSphere
+        radius={0.1}
+        widthSegments={90}
+        heightSegments={90}
+        texture={asset("chess-world.jpg")}
+        style={{
+          color: '#ffffff',
+          transform: [{translate: [0.75,0,-2]}, {rotateY: spinY}, {scale: [2, 2, 2]}],
+        }}
+      />
+
+      <AnimatedCylinder
+        radiusTop={0.2}
+        radiusBottom={0.2}
+        dimHeight={0.3}
+        segments={90}
+        texture={asset("chess-world.jpg")}
+        style={{
+          color: '#ffffff',
+          transform: [{translate: [-0.75,0,-2]}, {rotateX: spinY}, {rotateY: spinY},],
+        }}
+      />
       </View>
     );
   }
 };
 
-AppRegistry.registerComponent('WelcomeToVR', () => WelcomeToVR);
+AppRegistry.registerComponent('WelcomeToVR', () => animation);
