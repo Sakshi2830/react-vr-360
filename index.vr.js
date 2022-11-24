fa2ad04aeb78a9ea2c6f256166ddcd99fecc8a3f
Sakshi Tripathi <saktripathi@deloitte.com>
2022-11-24 23:19:35 +0530

    rotation and disco light formation

--- index.vr.js
+++ index.vr.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {  AppRegistry,  asset,  Pano,  View,  Animated, Model,PointLight, DirectionalLight, SpotLight} from 'react-vr';
+import {  AppRegistry,  asset,  Pano,  View,  Animated, Model,PointLight, DirectionalLight, SpotLight, AmbientLight} from 'react-vr';
 import { Easing } from 'react-native';
 
 
@@ -28,9 +28,47 @@ export default class animation extends React.Component
           easing: Easing.linear,
         }
 
-    ).start();
+    ).start(() => this.antiRotate());
   }
 
+  antiRotate(){
+    this.changeColor();
+    this.state.animateValue.setValue(360);
+    Animated.timing(this.state.animateValue,{
+      toValue:0,
+      duration:2500,
+      easing: Easing.linear
+    }).start(()=>this.rotate())
+
+  }
+
+  changeColor(){
+    let rn = Math.floor((Math.random()*5) +1)
+    if(rn == 1){
+      this.setState({
+        output:'red'
+    });}
+    else if(rn == 2){
+      this.setState({
+        output:'green',
+      })
+    }
+    else if(rn == 3){
+      this.setState({
+        output:'orange'
+      })
+    }
+    else if(rn == 4){
+      this.setState({
+        output:'blue'
+      })
+    }
+    else{
+      this.setState({
+        output:'violet'
+      })
+    }
+  }
  
   
    render() {
@@ -39,10 +77,11 @@ export default class animation extends React.Component
          <View>
            <Pano source={asset('lobby.jpeg')}/>
            <PointLight style={{color:"grey", transform:[{translate:[0,0,0]}]}}/>
-           <SpotLight angle={30} style={{
+           <SpotLight intensity={0.9} style={{color:this.state.output}}/>
+           {/* <SpotLight angle={30} style={{
             color:"black", transform:[{translate:[0,0,0]},{rotateX:90}]
-           }}/>
-           <DirectionalLight style={{color:"white", transform:[{rotateX: 90},{rotateY:45},{rotateZ:90}]}}/>
+           }}/> */}
+           {/* <DirectionalLight style={{color:"white", transform:[{rotateX: 90},{rotateY:45},{rotateZ:90}]}}/> */}
            <AnimatedModel 
            source={{
             obj:asset('boy.obj'),mtl:asset('boy.mtl')
